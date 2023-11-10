1a3529476198037d3e7109bffad9870f4b515c6c
Guilherme <gui-teixeirapimentel@hotmail.com>
2023-11-09 21:29:49 -0300

    Added first shared struct

--- src/SharedStructs.hpp
+++ src/SharedStructs.hpp
@@ -1,29 +1,26 @@
-struct Config {
-  std::string device;
-  std::pair<unsigned, unsigned> resolution;
-  std::array<double, 9> K_matrix;
-  std::vector<float> distortion_coeffients;
-  std::map<std::string, std::variant<uint16_t, std::string, bool>> parameters;
-};
+#include <stdint.h>
+#include <vector>
+#include <string>
 
-// Construct the object
-Config c{"/dev/video0", {640, 480}, 
-	 {223.28249888247538, 0.0, 152.30570853111396,
-	  0.0, 223.8756535707556, 124.5606000035353,
-	  0.0, 0.0, 1.0},
-	 {-0.44158343539568284, 0.23861463831967872, 0.0016338407443826572,
-	  0.0034950038632981604, -0.05239245892096022},
-	 {{"start_server", bool{true}},
-	  {"max_depth", uint16_t{5}},
-	  {"model_path", std::string{"foo/bar.pt"}}}};
 
-// Serialize
-std::vector<uint8_t> bytes;
-auto bytes_written = alpaca::serialize(c, bytes);
+namespace pimentel
+{
+    struct MessagePacket
+    {
+        enum class MessageType : uint8_t
+        {
+            AKNOWLEDGE = 0,
 
-// Deserialize
-std::error_code ec;
-auto object = alpaca::deserialize<Config>(bytes, ec);
-if (!ec) {
-  // use object
+            _EOF = 255
+        };
+
+        MessageType m_msgType;
+        std::vector<uint8_t> m_bytes;
+    };
+
+    struct AKNOWLEDGE
+    {
+        bool m_valid = false;
+        std::string m_message = "";
+    };
 }
